1b25752d8981f8a913a6ebdc7cfe0bb45c2d2842
Binjohn <43265566+Binjohn@users.noreply.github.com>
2025-10-03 03:49:46 +0800

    ReggioSignInHelper 0.3

--- ReggioSignInHelper.js
+++ ReggioSignInHelper.js
@@ -1,13 +1,23 @@
 /**
  * Reggio Sign In Helper
  *
- * @version 0.2
+ * @version 0.3
  */
 
 javascript: (function () {
+    // #ifndef // prevent re-injection
+    if (typeof(bjtLocked) !== "undefined") { return; }
+    window.bjtLocked = true;
+    
+    // inject
     var el = document.getElementById("attendances_newinput_2in1");
     el.type = "password"; // disable IME
     lock();
+    
+    // highlight to indicate injection state
+    el.style.border = "3px solid limegreen";
+    el.style.borderRight = "none";
+    el.style.backgroundColor = "honeydew";
     
     // lock the focus to the input field
     window.addEventListener("focus", lock);
@@ -22,8 +32,9 @@ javascript: (function () {
             return;
         } else {
             // anything else is clicked
-            el.value = "   ";
-            el.focus();
+            el.focus(); // focus last to ensure the cursor blinks
+            el.value = "   "; // avoid password auto-complete
+            el.select(); // select all text
         }
     }
 })();
@@ -31,9 +42,14 @@ javascript: (function () {
 /**
  * changelog
  *
+ * @version 0.3 2025-10-03
+ * x Selects all text in the input field for intuitiveness.
+ * x Prevents re-injection.
+ * * Highlights the input field as injection indicator.
+ *
  * @version 0.2 2025-10-02
- * + Avoid password autocomplete by set the value to three white spaces.
- * + Exclude when the button or the input field is clicked
+ * + Avoids password autocomplete by inserting white spaces.
+ * + Excludes clicks on the button or the input field.
  *
  * @version 0.1 2025-10-01
  */
